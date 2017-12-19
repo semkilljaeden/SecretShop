430bf1d74a780d386929480e778353e87d4d56c7
hw72786 <hw72786@APSGONTW7W0924.apac.nsroot.net>
2017-12-19 17:39:07 +0800

    first commit

---
+++ SecretShop.Bot/BotActor.cs
@@ -0,0 +1,50 @@
+﻿using Akka.Actor;
+using SteamKit2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretShop.Bot
+{
+    class BotActor : ReceiveActor
+    {
+        private SteamClient _client;
+        private CallbackManager _callBackManager;
+        private SteamUser _userHandler;
+        private SteamFriends _friendsHandler;
+        private SteamGameCoordinator _coordinatorHandler;
+        private List<SteamID> _friends;
+
+
+        public IEnumerable<SteamID> FriendsList
+        {
+            get
+            {
+                CreateFriendsListIfNecessary();
+                return _friends;
+            }
+        }
+
+        public BotActor()
+        {
+            _client = new SteamClient();
+            _callBackManager = new CallbackManager(_client);
+            _userHandler = _client.GetHandler<SteamUser>();
+            _friendsHandler = _client.GetHandler<SteamFriends>();
+            _coordinatorHandler = _client.GetHandler<SteamGameCoordinator>();
+        }
+
+
+        private void CreateFriendsListIfNecessary()
+        {
+            if (_friends != null)
+                return;
+
+            _friends = new List<SteamID>();
+            for (int i = 0; i < _friendsHandler.GetFriendCount(); i++)
+                _friends.Add(_friendsHandler.GetFriendByIndex(i));
+        }
+    }
+}
